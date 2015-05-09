6664c0b3a1fcdc892c58d5ed846fc357da174f37
Eddy Petrișor <eddy.petrisor@gmail.com>
2015-05-09 10:35:02 +0300

    cleanbuild_bisect.sh: dev system = host
    Add a modified version of the cleanbuild.sh script better suited for the
    situation when the development system is also the TFTP/NFS host used by the
    NSLU2 under NetBSD.
    
    Signed-off-by: Eddy Petrișor <eddy.petrisor@gmail.com>

---
+++ cleanbuild_bisect.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+set -e
+
+git clean -x -f -d
+git reset --hard HEAD
+
+cp ../NSLU2_ALL sys/arch/evbarm/conf/
+cp ../IxNpeMicrocode.dat sys/arch/arm/xscale/
+
+#patch -p1 < ../binutils.patch
+export NOGCCERROR=yes
+
+export ver=$(git describe --tags)
+
+git diff
+
+(
+./build.sh -j 3 -U -m evbarm -a armeb -V NOGCCERROR=yes tools              &&
+./build.sh -j 3 -u -U -m evbarm -a armeb -V NOGCCERROR=yes build           &&
+./build.sh -j 3 -u -U -m evbarm -a armeb -V KERNEL_SETS=NSLU2_ALL release
+) 2>&1 | tee ../log-$ver.txt | grep -q 'extra files in DESTDIR' && exit 0 || true
+
+tail -n 1000 ../log-$ver.txt | grep -q 'Successful make release' && exit 1 || exit 125
+
+#ls -l obj/releasedir/evbarm/binary/sets
