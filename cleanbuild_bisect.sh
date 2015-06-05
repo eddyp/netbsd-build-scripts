5cb59da0d2ebc318842b8541bfedf5778e7d2722
Eddy Petrișor <eddy.petrisor@gmail.com>
2015-06-05 08:34:07 +0300

    use a 'namespace' for script vars, maybe this is the problem with sets
    Signed-off-by: Eddy Petrișor <eddy.petrisor@gmail.com>

--- cleanbuild_bisect.sh
+++ cleanbuild_bisect.sh
@@ -5,11 +5,11 @@ par=3
 while [ $# -gt 0 ]; do
 	case $1 in
 		-t)
-			toolbuild=1;
+			cb_toolbuild=1;
 			shift;
 			;;
 		-k)
-			kernbuild=1;
+			cb_kernbuild=1;
 			shift
 			;;
 		-j)
@@ -24,10 +24,10 @@ while [ $# -gt 0 ]; do
 		-l)
 			shift
 			if [ "$1" ]; then
-				logsuf="-$1"
+				cb_logsuf="-$1"
 				shift
 			else
-				logsuf=-bisect
+				cb_logsuf=-bisect
 			fi
 			;;
 		*)
@@ -37,7 +37,7 @@ while [ $# -gt 0 ]; do
 	esac
 done
 
-[ "$toolbuild" ] || mv obj/tooldir.Linux-3.18.9-gd1034e83-heidi-x86_64 ../
+[ "$cb_toolbuild" ] || mv obj/tooldir.Linux-3.18.9-gd1034e83-heidi-x86_64 ../
 git clean -x -f -d || [ "$(git status -u -s | wc -l)" -eq 0 ]
 git reset --hard HEAD
 
@@ -51,7 +51,7 @@ patch -p0 < ../netbsd-elf.diff || true
 patch -p1 < ../crypto.inc.diff || true
 #export NOGCCERROR=yes
 
-export ver=$(git describe --tags | tr '/' '_')
+export cb_ver=$(git describe --tags | tr '/' '_')
 #export SLOPPY_FLIST=${SLOPPY_FLIST-yes}
 #export MKMAN=${MKMAN-no}
 #export MKDOC=${MKDOC-no}
@@ -71,36 +71,36 @@ git diff
 #./build.sh -j 3 -U -m evbarm -a armeb -V NOGCCERROR=yes tools              &&
 #./build.sh -j 3 -u -U -m evbarm -a armeb -V NOGCCERROR=yes build           &&
 #./build.sh -j 3 -u -U -m evbarm -a armeb -V KERNEL_SETS=NSLU2_ALL release
-#) 2>&1 | tee ../log-$ver.txt | grep -q 'extra files in DESTDIR' && exit 0 || true
-if [ "$kernbuild" ]; then
-	#DISTR=release
-	DISTR=distribution
-	#VAR="-V KERNEL_SETS=NSLU2_ALL -V BUILD_KERNELS=NSLU2_ALL"
-	#VAR="-V KERNEL_SETS=NSLU2_ALL -V ALL_KERNELS=NSLU2_ALL"
-	#VAR="-V ALL_KERNELS=NSLU2_ALL"
-	#VAR="-V ALL_KERNELS=NSLU2_ALL kernel=NSLU2_ALL"
-	VAR="kernel=NSLU2_ALL"
-	logsuf="-kern$logsuf"
+#) 2>&1 | tee ../log-$cb_ver.txt | grep -q 'extra files in DESTDIR' && exit 0 || true
+if [ "$cb_kernbuild" ]; then
+	#cb_distr=release
+	cb_distr=distribution
+	#cb_var="-V KERNEL_SETS=NSLU2_ALL -V BUILD_KERNELS=NSLU2_ALL"
+	#cb_var="-V KERNEL_SETS=NSLU2_ALL -V ALL_KERNELS=NSLU2_ALL"
+	#cb_var="-V ALL_KERNELS=NSLU2_ALL"
+	#cb_var="-V ALL_KERNELS=NSLU2_ALL kernel=NSLU2_ALL"
+	cb_var="kernel=NSLU2_ALL"
+	cb_logsuf="-kern$cb_logsuf"
 else
-	DISTR=distribution
-	logsuf="-nokern$logsuf"
+	cb_distr=distribution
+	cb_logsuf="-nokern$cb_logsuf"
 fi
 (
 mkdir -p obj &&
 (
-	if [ $toolbuild ]; then
+	if [ $cb_toolbuild ]; then
 		./build.sh -j 3 -u -U -m evbarm -a armeb tools ;
 	else
 		mv ../tooldir.Linux-3.18.9-gd1034e83-heidi-x86_64 obj/
 	fi
 ) &&
 ./build.sh -j $par -u -U -m evbarm -a armeb -V NOGCCERROR=yes build          &&
-./build.sh -j $par -u -U -m evbarm -a armeb $VAR $DISTR &&
+./build.sh -j $par -u -U -m evbarm -a armeb $cb_var $cb_distr &&
 ./build.sh -j $par -u -U -m evbarm -a armeb sets
-) 2>&1 | tee ../log$logsuf-$ver.txt || true
-#) 2>&1 | tee ../log-$ver.txt | grep -q 'extra files in DESTDIR' && exit 0 || true
+) 2>&1 | tee ../log$cb_logsuf-$cb_ver.txt || true
+#) 2>&1 | tee ../log-$cb_ver.txt | grep -q 'extra files in DESTDIR' && exit 0 || true
 
-#tail -n 1000 ../log-$ver.txt | grep -q 'Successful make release' && exit 1 || exit 125
-tail -n 1000 ../log$logsuf-$ver.txt | grep -q 'Built sets to /home/eddy/usr/src/netbsd/net/src/obj/releasedir/evbarm/binary/sets' && exit 1 || exit 125
+#tail -n 1000 ../log-$cb_ver.txt | grep -q 'Successful make release' && exit 1 || exit 125
+tail -n 500 ../log$cb_logsuf-$cb_ver.txt | grep -q 'Built sets to /home/eddy/usr/src/netbsd/net/src/obj/releasedir/evbarm/binary/sets' && exit 1 || exit 125
 
 #ls -l obj/releasedir/evbarm/binary/sets
